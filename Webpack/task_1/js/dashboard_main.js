5ca9f6b1cdb1e9bb0d9e5f884fac16587063af41
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-03-12 16:10:26 -0500

    add dashboard

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,39 +1,18 @@
-// dashboard_main.js
-
-// Import jQuery
 import $ from 'jquery';
-// Import debounce function from lodash
-import debounce from 'lodash/debounce';
+import _ from 'lodash';
 
-// Function to create and append elements
-function createDashboardElements() {
-    // Create and append the first paragraph
-    $('<p>').text('Holberton Dashboard').appendTo('body');
-
-    // Create and append the second paragraph
-    $('<p>').text('Dashboard data for the students').appendTo('body');
-
-    // Create and append the button element
-    $('<button>').text('Click here to get started').appendTo('body');
-
-    // Create and append the paragraph with id 'count'
-    $('<p>').attr('id', 'count').appendTo('body');
-
-    // Create and append the last paragraph
-    $('<p>').text('Copyright - Holberton School').appendTo('body');
-}
-
-// Function to update counter
 let count = 0;
 function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
+  count++;
+  $('#count').text(`${count} clicks on the button`);
 }
 
-// Call the function to create and append elements when the document is ready
-$(document).ready(function() {
-    createDashboardElements();
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button type="button">Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-    // Bind debounce function to button click event
-    $('button').on('click', debounce(updateCounter, 500));
-});
+const debouncedUpdate = _.debounce(updateCounter, 500)
+
+$('button').on('click', debouncedUpdate)
