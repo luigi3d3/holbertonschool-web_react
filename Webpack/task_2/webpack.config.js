a917f06b7ce0d8daeda688cba896b305368a2a3a
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-03-12 19:13:27 -0500

    ejercicios

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -4,7 +4,30 @@ module.exports = {
   mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        use: [
+          'file-loader', // Use file-loader for basic image handling
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
