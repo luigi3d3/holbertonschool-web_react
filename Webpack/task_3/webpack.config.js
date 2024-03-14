6f2d6acfd58509add477dff168e6449f7784a1b9
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-03-13 23:13:23 -0500

    add webpack

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,4 +1,3 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -12,40 +11,5 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65,
-              },
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html', // Path to your HTML template
-      filename: 'index.html', // Name of the generated HTML file
-    }),
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    compress: true,
-    port: 8564,
-    open: true,
   },
 };
