1f4756b22314ffcc0ebb32dfcb534747448c9e21
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-03-13 23:15:35 -0500

    add webpack

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -11,5 +12,17 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // Path to your HTML template
+      filename: 'index.html', // Name of the generated HTML file
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 8564,
+    open: true,
   },
 };
