c08176c650fc68f98e161affb33c706ad3b8a375
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-06-30 18:13:15 -0500

    Commence with class components

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -16,19 +16,16 @@ class App extends Component {
 
   render() {
     const { isLoggedIn } = this.props;
-
     const listCourses = [
       { id: 1, name: 'ES6', credit: 60 },
       { id: 2, name: 'Webpack', credit: 20 },
       { id: 3, name: 'React', credit: 40 },
     ];
-
     const listNotifications = [
       { id: 1, type: 'default', value: 'New course available' },
       { id: 2, type: 'urgent', value: 'New resume available' },
       { id: 3, type: 'default', value: 'New data available' },
     ];
-
     return (
       <div className="App">
         <Notifications listNotifications={listNotifications} />
