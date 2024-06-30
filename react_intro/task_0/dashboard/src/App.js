e83c53e37278d7cd7df9ea8301545943790ed1d5
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-06-30 13:26:50 -0500

    Basic application

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,23 @@
+// task_0/dashboard/src/App.js
+import React from 'react';
+import logo from './holberton-logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright 2024 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
