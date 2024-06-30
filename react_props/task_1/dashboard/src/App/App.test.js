3cc895444aff63412034a78e0b50b681e1e1939d
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-06-30 13:52:03 -0500

    Write the tests for each component

---
+++ react_props/task_1/dashboard/src/App/App.test.js
@@ -0,0 +1,30 @@
+// Import necessary modules
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+
+describe('App Component', () => {
+  it('should contain the Notifications component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Notifications)).toHaveLength(1);
+  });
+
+  it('should contain the Header component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header)).toHaveLength(1);
+  });
+
+  it('should contain the Login component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Login)).toHaveLength(1);
+  });
+
+  it('should contain the Footer component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Footer)).toHaveLength(1);
+  });
+});
---
+++ react_props/task_1/dashboard/src/Footer/Footer.test.js
@@ -0,0 +1,16 @@
+// Import necessary modules
+import React from 'react';
+import { shallow } from 'enzyme';
+import Footer from './Footer';
+
+describe('Footer Component', () => {
+  it('should render without crashing', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('should render at least the text "Copyright"', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.text()).toContain('Copyright');
+  });
+});
