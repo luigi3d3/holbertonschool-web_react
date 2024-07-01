67a741c5d4587402c1918db8e328e0e721eb9183
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-07-01 02:30:55 -0500

    Introduction to react hook

---
+++ react_state/task_4/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+
+const styles = StyleSheet.create({
+  row: {
+    backgroundColor: '#f5f5f5ab',
+  },
+  header: {
+    backgroundColor: '#deb5b545',
+  },
+  th: {
+    fontWeight: 'bold',
+  },
+  rowChecked: {
+    backgroundColor: '#e6e4e4',
+  },
+});
+
+
+const CourseListRow = ({ textFirstCell, textSecondCell, isHeader }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const rowStyle = css(isHeader ? styles.header : styles.row, isChecked && styles.rowChecked);
+
+  return (
+    <tr className={rowStyle}>
+      {isHeader ? (
+        <>
+          <th className={css(styles.th)}>{textFirstCell}</th>
+          <th className={css(styles.th)}>{textSecondCell}</th>
+        </>
+      ) : (
+        <>
+          <td>
+            <input type="checkbox" onChange={handleCheckboxChange} checked={isChecked} />
+            {textFirstCell}
+          </td>
+          <td>{textSecondCell}</td>
+        </>
+      )}
+    </tr>
+  );
+};
+
+
+CourseListRow.propTypes = {
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  isHeader: PropTypes.bool,
+};
+
+
+CourseListRow.defaultProps = {
+  textSecondCell: null,
+  isHeader: false,
+};
+
+
+export default CourseListRow;
